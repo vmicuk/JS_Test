d786f24e86b8abaadb3682eb2dca938f631033cf
vmicuk <vitalij.micuk@gmail.com>
2022-01-18 19:42:51 +0200

    Add Task 9

---
+++ Task9/script.js
@@ -0,0 +1,8 @@
+/* ------------------------------ TASK 9 ---------------------------------------------------------------
+Sukurkite konstruktoriaus funkciją "Movie" (naudokte ES6), kuri sukuria objektus 3 savybėm ir 1 metodu:
+
+Savybės:
+title, director, budget
+Metodas: 
+wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, tada gražins true, kiru atveju false 
+------------------------------------------------------------------------------------------------------ */
