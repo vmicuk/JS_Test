0cf19dd4a5e091e5e44dd6011eda3d69253d480f
vmicuk <vitalij.micuk@gmail.com>
2022-01-18 19:38:50 +0200

    Add Task 8

---
+++ Task8/script.js
@@ -0,0 +1,7 @@
+/* ------------------------------ TASK 8 --------------------------------------------
+Sukurkite konstruktoriaus funkciją "Calculator" (naudokite ES5), kuri sukuria objektus su 3 metodais:
+sum() - priima du skaičius ir grąžina jų sumą.
+subtraction() - priima du skaičius ir grąžina jų skirtumą.
+multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
+division() - priima du skaičius ir grąžina jų dalybos rezultatą;
+------------------------------------------------------------------------------------ */
